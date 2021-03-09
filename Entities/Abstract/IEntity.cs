490d270cd3748cc238a9eb980be6f41c5068df32
enisarisin <enisarisin@gmail.com>
2021-03-09 13:50:46 +0300

    Add files via upload

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Class that implenment IEntity is a database table.
+    public interface IEntity
+    {
+    }
+}
