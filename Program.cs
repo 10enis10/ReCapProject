8bd227883b2089dc6dbeaaa4172f39bad820a1d5
enisarisin <enisarisin@gmail.com>
2021-03-09 13:51:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car { Id = 1, BrandId = 5, ColorId = 3, DailyPrice = 320, ModelYear = "2018", Description = "Ford Fiesta" };
+            InMemoryCarDal inMemoryCarDal = new InMemoryCarDal();
+            //inMemoryCarDal.Add(car);
+            //inMemoryCarDal.Delete(car);
+            inMemoryCarDal.Update(car);
+            inMemoryCarDal.GetById(car);
+        }
+    }
+}
